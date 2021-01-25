9efd27835bc05c56fd36cce45c36041e144d8a30
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-01-25 20:26:00 +0800

    新增createServer_bedrockDedicatedServer，用于创建可用的服务器 但是仍然不能正常使用

--- bdsh.sh
+++ bdsh.sh
@@ -10,6 +10,7 @@ WD="$(pwd)"
 BDSH="${WD}/.bds"
 TMP="${BDS}/tmp"
 CONFIG="${BDS}/config"
+ASSETS="${BDS}/assets"
 VERSION="bdsh-8.0 build"
 BDS_ASSETS="${BDS}/assets"
 #################
@@ -91,12 +92,69 @@ bedrockDedicatedServerProcess () {
 ##################
 updateAssets_bedrockDedicatedServer () {
     ASSET_BEDROCK_DEDICATED_SERVER_LATEST="$(wget -qO - 'https://www.minecraft.net/en-us/download/server/bedrock'|grep -oe 'https://minecraft.azureedge.net/bin-linux/bedrock-server-.*.zip')"
-    ASSET_BEDROCK_DEDICATED_SERVER_LATEST_VERSION="$(echo ${BDS_LATEST_ASSET}|sed -e 's/.*-//g' -e 's/\.zip//')"
+    if [[ ! ${ASSET_BEDROCK_DEDICATED_SERVER_LATEST} ]]; then
+        printf "\e[33m获取最新版本时出错\e[0m\n"
+        return 1
+    fi
+    ASSET_BEDROCK_DEDICATED_SERVER_LATEST_VERSION="$(echo ${ASSET_BEDROCK_DEDICATED_SERVER_LATEST}|sed -e 's/.*-//g' -e 's/\.zip//')"
 }
 config_map () {
     find "$WD" -maxdepth 2 -regex "$WD/[^.].*?/.bdsh_config" -type f | while read c; do
         cd $(basename "${c}")
     done
+}
+createServer_bedrockDedicatedServer () {
+    local server="$1"
+    local serverVersion="$2"
+    if [[ -n ${server} ]]; then
+        if [[ ! -e ${server} ]]; then
+            printf "创建服务器${server}\n"
+            if [[ -n ${serverVersion} ]]; then
+                if [[ -f ${ASSETS}/bedrockDedicatedServer/${serverVersion} ]]; then
+                    printf "版本: ${severVersion}\n"
+                else
+                    printf "\e[31m版本: ${serverVersion}不存在\e[0m\n"
+                    return 1
+                fi
+            else
+                printf "未指定版本，使用最新版本\n"
+                if [[ ! ${ASSET_BEDROCK_DEDICATED_SERVER_LATEST_VERSION} ]]; then
+                    printf "正在检查版本\n"
+                    updateAssets_bedrockDedicatedServer
+                fi
+                serverVersion="${ASSET_BEDROCK_DEDICATED_SERVER_LATEST_VERSION}"
+                printf "最新版本为${serverVersion}\n"
+            fi
+            mkdir "${server}"
+            cd "${server}"
+            printf "解压资源\n请确保有足够的空间"
+            local c=0
+            unzip -qo "${ASSETS}/bedrockDedicatedServer/${serverVersion}"
+            if [[ $? != 0 ]]; then
+                printf "解压出现错误，是否继续？[Y/n,默认为否]"
+                c=1
+            fi
+            if [[ ${c} = 0 ]] || (read c; if [[ ${c} =~ ^[Y|y]$ ]]; then exit 0; else exit 1; fi); then
+                mkdir .bdsh
+                printf "#This is config for BDSH\n#Please don't modify it at will unless you know what it means.\ncreateVersion=${VERSION}\ntype=bedrockDedicatedServer\nserver=${server}\nserverVersion=${serverVersion}" >.bdsh/config
+                mv bedrock_server .bdsh/setup
+                printf "'${server}'创建完成\n版本: ${serverVersion}\n"
+            else
+                cd "${WD}"
+                rm -rf "${server}"
+                printf "取消创建\n" >&2
+                return 2
+            fi
+        else 
+            printf "无法创建，因为${server}已存在\n" >&2
+            return 1
+        fi
+    else 
+        echo "你需要为创建bedrockDedicatedServer指定一个名字"
+        echo "用法: $FUNCNAME <名字> [版本]"
+        echo "如不指定，默认使用 \${ASSET_BEDROCK_DEDICATED_SERVER_LATEST_VERSION} "
+        return 1
+    fi
 }
 #################
 #################
