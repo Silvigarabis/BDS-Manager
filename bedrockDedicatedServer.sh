3a4d75456fcbac5521f25116e3a8d3910606861d
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-02-08 23:57:22 +0800

    something be added

--- bedrockDedicatedServer.sh
+++ bedrockDedicatedServer.sh
@@ -1,6 +1,6 @@
 bedrockDedicatedServer () { #服务器启动/关闭/发送命令/重启 等
     #启动守护进程
-    if [[ ! ${BDS_DAEMON_PROCESS} ]]; then
+    if [[ ! ${BDS_DAEMON} ]]; then
         bedrockDedicatedServerDaemon #服务器守护进程（等待中）
     fi
     #处理基础变量
@@ -10,8 +10,8 @@ bedrockDedicatedServer () { #服务器启动/关闭/发送命令/重启 等
     case "${option}" in #判断操作
         start) #操作：启动服务器 设置变量 创建输入输出文件
         local directory="${3:-.}"
-        local input="${tmp}/input.${name}${RANDOM}"
-        local output="${tmp}/output.${name}${RANDOM}"
+        local input="${tmp}/bds/input.${name}${RANDOM}"
+        local output="${tmp}/bds/output.${name}${RANDOM}"
         local binary="${4:-${latest_binary:-\$undefined}}"
         mkdir -p "${cmd%/*}" "${out%/*}"
         : > "${input}"
@@ -22,17 +22,17 @@ bedrockDedicatedServer () { #服务器启动/关闭/发送命令/重启 等
             return 127
         else #启动
             tail -F "${input}" | "${binary}" > "${output}" &
-            echo "('${name}' '$!' 'cmd=${input}' out='${output}')" > "${BDS_LIST}"
+            echo "server=('${name}' '$!' 'cmd=${input}' out='${output}')" > "${BDS_LIST}"
         fi;;
         stop) #操作：终止或暂停一个服务器
         local li e server
         while read li; do
-            e=${li}
+            eval "${li}"
             if [[ ${e} = ${name} ]]; then
                 server=(${e[@]})
                 break
             fi
-        done < "${BDS_LIST}"
+        done < 
         if [[ -z ${server} ]]; then
             echo 没有找到指定的服务器 >&2
             return 1
@@ -53,4 +53,29 @@ bedrockDedicatedServer () { #服务器启动/关闭/发送命令/重启 等
             kill -${signal} ${server[2]}
         fi;;
     esac
+}
+bedrockDedicatedServerDaemon () {
+    BDSLIST="${tmp}/${FUNCNAME}.${$}-${RANDOM}"
+    while [[ -e "${BDSLIST}" ]]; do
+        BDSLIST="${tmp}/${FUNCNAME}.${$}-${RANDOM}"
+    done
+    BDSDAEMON="${BDSLIST}.DAEMON"
+    {
+        trap "signal=0" 41
+        trap "signal=0" 42
+        for ((;;)); do
+            echo "DAEMON_PROCESS=$$" > "${BDSDAEMON}"
+            num=0
+            while read li; do
+                let num++
+                eval "${li}"
+                if ps ${server[1]} |grep ${server[1]}; then
+                    status=1
+                else status=0
+                fi
+                list[${num}]="server=(${server[@]} ${status})"
+                
+            done
+        done < "${BDSLIST}"
+    } &
 }
