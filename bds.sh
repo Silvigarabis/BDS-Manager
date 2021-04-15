a126b9cd341e676033ae0600e7f5217790c24de0
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-04-15 21:20:23 +0800

    0

--- bds.sh
+++ bds.sh
@@ -1,2 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 WD="${PWD}"
+
+get-latest-version() {
+  if [[ $# = 0 ]]; then
+    wget -qO- 'https://www.minecraft.net/en-us/download/server/bedrock'
+    if [[ $? != 0 ]]; then
+      get-latest-version -v
+    fi
+  elif [[ $1 = --no-check-certificate ]]; then
+    wget --no-check-certificate -qO- 'https://www.minecraft.net/en-us/download/server/bedrock'
+  else
+    wget -O- 'https://www.minecraft.net/en-us/download/server/bedrock' "$@"
+  fi | sed -n 's/.*bin-linux\/bedrock-server-\(.*\).zip.*/\1/p'
+}
+
+"$@"
