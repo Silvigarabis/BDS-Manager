136c3d3b5d2a5cc7b3a6aac354f7c9c1a730cb01
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-06-24 06:26:44 +0800

    [commit]change way to download file

--- bds.sh
+++ bds.sh
@@ -1,17 +1,14 @@
-#!/usr/bin/env bash
-WD="${PWD}"
+#!/bin/bash
+
+ORIGIN=${ORIGIN:-$(cat "$0")}
+WD=${WD:-${PWD}}
 
 get-latest-version() {
-  if [[ $# = 0 ]]; then
-    wget -qO- 'https://www.minecraft.net/en-us/download/server/bedrock'
-    if [[ $? != 0 ]]; then
-      get-latest-version -v
-    fi
-  elif [[ $1 = --no-check-certificate ]]; then
-    wget --no-check-certificate -qO- 'https://www.minecraft.net/en-us/download/server/bedrock'
-  else
-    wget -O- 'https://www.minecraft.net/en-us/download/server/bedrock' "$@"
-  fi | sed -n 's/.*bin-linux\/bedrock-server-\(.*\).zip.*/\1/p'
+  local update_file=$(curl 'https://www.minecraft.net/en-us/download/server/bedrock')
+  sed -nE 's@.*bin-linux/bedrock-server-(.*)\.zip.*@\1@p' <<EOF
+${update_file}
+EOF
 }
 
 "$@"
+
