b63a1b0072773829bf105f7f7435cc71b0ec92c5
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-06-24 10:33:30 +0800

    [m]

--- bds.sh
+++ bds.sh
@@ -3,14 +3,21 @@
 ORIGIN=${ORIGIN:-$(cat "$0")}
 WD=${WD:-${PWD}}
 
-getLatestVersion(){
+get_latest_version(){
   if ! curl 'https://www.minecraft.net/en-us/download/server/bedrock'; then
-    printf "Err: cannot download file from 'https://www.minecraft.net/en-us/download/server/bedrock'\n" >&2
+    printf "[Update] Err: cannot download file from 'https://www.minecraft.net/en-us/download/server/bedrock'\n" >&2
   fi | sed -nE 's@.*bin-linux/bedrock-server-(.*)\.zip.*@\1@p'
 }
 
 version(){
   case "$1" in
-  
+    check-update)
+      printf "[Update] Download Version Document File\n" >&2
+      {
+        get_latest_version
+      }
+    ;;
+  esac
 }
+
 "$@"
